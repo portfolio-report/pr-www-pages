094a1234b46067cee492c48f721effc5c4c79d86
tfabritius <tfabritius@users.noreply.github.com>
2022-03-26 08:13:13 +0000

    deploy: portfolio-report/pr-www@4dea54325c6b72a5fa8408d3cc5b466239fd4e33

---
+++ _nuxt/static/1648282389/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648282389",layout:"default",error:a,state:{"security.model":{},auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Fcontact",config:{axios:{browserBaseURL:void 0},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
