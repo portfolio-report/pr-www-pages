6332fb0393be5ba82e82ab6b73372f5c8e1b53f9
tfabritius <tfabritius@users.noreply.github.com>
2022-04-25 14:15:09 +0000

    deploy: portfolio-report/pr-www@b8f95d1b0766cd29e31935bff1115445838a045f

--- _nuxt/static/1650891656/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650891656",layout:"default",error:a,state:{"security.model":{},auth:{user:a,loggedIn:b,strategy:"local"}},serverRendered:b,routePath:c,config:{axios:{browserBaseURL:void 0},_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null,false,"\u002F"));
