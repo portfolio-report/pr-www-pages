0feaf88a926d21fae03cae76d97b513620729c97
tfabritius <tfabritius@users.noreply.github.com>
2023-10-28 08:23:03 +0000

    deploy: portfolio-report/pr-www@daf527d26699fb7dc5b87bbb4162b8e7b6e5bd3a

--- _nuxt/index.cbfbcd02.js
+++
@@ -1,1 +0,0 @@
-import{h as e,A as a,m as t,t as n}from"./entry.0e080798.js";const _=e({__name:"index",setup(o){return a("/search"),(r,s)=>(n(),t("div"))}});export{_ as default};
--- _nuxt/useApi.79b2a229.js
+++
@@ -1,1 +0,0 @@
-import{Y as s}from"./entry.0e080798.js";async function a(e,i){const n=s();return await $fetch(e,{baseURL:n.public.baseUrl,...i})}export{a as u};
