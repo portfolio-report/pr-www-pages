1b1f7c9a54de3c97029ed667a50b8d8a69389ceb
tfabritius <tfabritius@users.noreply.github.com>
2023-11-17 20:22:27 +0000

    deploy: portfolio-report/pr-www@08dc9d3ffd62162367dd85574f7bf1f85db632f8

---
+++ _nuxt/useApi.0fad9375.js
@@ -0,0 +1,1 @@
+import{Y as s}from"./entry.201b2277.js";async function a(e,i){const n=s();return await $fetch(e,{baseURL:n.public.baseUrl,...i})}export{a as u};
--- _nuxt/useApi.f55ac073.js
+++
@@ -1,1 +0,0 @@
-import{Y as s}from"./entry.eb62e96c.js";async function a(e,i){const n=s();return await $fetch(e,{baseURL:n.public.baseUrl,...i})}export{a as u};
