124f8d7bb9fbb64d3c327dba593cb8b192ee0664
tfabritius <tfabritius@users.noreply.github.com>
2024-04-26 20:25:41 +0000

    deploy: portfolio-report/pr-www@4faacd7373aaa851a92d01092ffc819048e861cc

--- _nuxt/Cx90whnQ.js
+++
@@ -1,1 +0,0 @@
-import{a0 as n}from"./Bslm0le2.js";async function t(a,e){const i=n();return await $fetch(a,{baseURL:i.public.baseUrl,...e})}export{t as u};
---
+++ _nuxt/DeD5-ON8.js
@@ -0,0 +1,1 @@
+import{h as e,A as a,m as t,t as n}from"./Cea_MXTp.js";const _=e({__name:"index",setup(o){return a("/search"),(r,s)=>(n(),t("div"))}});export{_ as default};
